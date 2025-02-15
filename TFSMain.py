e0d8fd3cac2e8503cf8f181da629db1e3df5335b
stefanodesaraca <stefanodesaraca@gmail.com>
2025-02-15 16:23:29 +0100

    Main creation

---
+++ TFSMain.py
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    while True:
+        print("1. ")
+        print("0. Exit")
+
+        option = input("Choice: ")
+        print()
+
+        if option == "1":
+            pass
+
+        elif option == "0":
+            exit()
+
+        else:
+            print("Wrong option. Insert a valid one")
+            print()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
