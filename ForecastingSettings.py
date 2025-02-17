7a99c304bf24eba288aba3437fd7e26413fe3fe5
stefanodesaraca <stefanodesaraca@gmail.com>
2025-02-17 13:02:14 +0100

    Forecasting datetime code

--- ForecastingSettings.py
+++ ForecastingSettings.py
@@ -1,14 +1,38 @@
+from datetime import datetime
+
+target_datetime_filename = "target_datetime"
+dt_format = '%Y %m %d %H:%-M%-S'  # Datetime format, the hour (H) must be zero-padded and 24-h base, for example: 01, 02, ..., 12, 13, 14, 15, etc.
 
 
+def write_forecasting_target_datetime():
+
+    dt = input("Insert Target Datetime (YYYY-MM-DD hh:mm): ") #The month number must be zero-padded, for example: 01, 02, etc.
+
+    formatted_datetime = datetime.strptime(dt, dt_format)
+    print("Target Datetime: ", formatted_datetime)
+
+    with open(f"{target_datetime_filename}.txt", "w") as t_dt_writer:
+        t_dt_writer.write(str(formatted_datetime))
+
+    return None
 
 
+def read_forecasting_target_datetime():
+
+    #target_dt = None
+
+    try:
+        with open(f"{target_datetime_filename}.txt", "r") as t_dt_reader:
+
+            target_dt = t_dt_reader.read()
+            target_dt = datetime.strptime(target_dt, dt_format)
+
+    except FileNotFoundError:
+        print("\033[91mTarget Datetime File Not Found\033[0m")
+        exit()
 
 
-
-
-
-
-
+    return target_dt
 
 
 
--- OpsSettings.py
+++ OpsSettings.py
@@ -1,13 +1,13 @@
 import os
 from cleantext import clean
 
-
+current_ops_filename = "current_ops"
 
 def write_current_ops_file(ops_name: str):
 
     ops_name = clean(ops_name, to_ascii=True, no_emoji=True)
 
-    with open("current_ops.txt", "w") as ops_file:
+    with open(f"{current_ops_filename}.txt", "w") as ops_file:
         ops_file.write(ops_name)
 
     return None
@@ -18,7 +18,7 @@ def write_current_ops_file(ops_name: str):
 def read_current_ops_file():
 
     try:
-        with open("current_ops.txt", "r") as ops_file:
+        with open(f"{current_ops_filename}.txt", "r") as ops_file:
             op = ops_file.read()
 
     except FileNotFoundError:
