54b76639e34837571b3849f21c74a41410fa9faf
stefanodesaraca <stefanodesaraca@gmail.com>
2025-02-19 15:09:59 +0100

    Menu graphic adjustment

--- TFSMain.py
+++ TFSMain.py
@@ -52,25 +52,25 @@ def manage_ops(functionality: str):
 def main():
     while True:
         print("""==================== MENU ==================== 
-                 1. Set pre-analysis information
-                    1.1 Create an operation
-                    1.2 Set an operation as active (current one)
-                    1.3 Check the active operation name
-                 2. Download traffic volumes data (Trafikkdata API)
-                 3. Forecast
-                    3.1 Set forecasting target datetime
-                    3.2 Forecast warmup
-                    3.3 Execute forecast
-                        3.3.1 One-Point Forecast
-                        3.3.2 A2B Forecast
-                 4. Road network graph generation
-                 5. Other options
-                    5.1 Set forecasting system folders (manually)
-                    5.2 EDA (Exploratory Data Analysis)
-                    5.3 Erase all data about an operation
-                    5.4 Find best model for the current operation
-                    5.5 Analyze pre-existing road network graph
-                 0. Exit""")
+1. Set pre-analysis information
+    1.1 Create an operation
+    1.2 Set an operation as active (current one)
+    1.3 Check the active operation name
+ 2. Download traffic volumes data (Trafikkdata API)
+ 3. Forecast
+    3.1 Set forecasting target datetime
+    3.2 Forecast warmup
+    3.3 Execute forecast
+        3.3.1 One-Point Forecast
+        3.3.2 A2B Forecast
+ 4. Road network graph generation
+ 5. Other options
+    5.1 Set forecasting system folders (manually)
+    5.2 EDA (Exploratory Data Analysis)
+    5.3 Erase all data about an operation
+    5.4 Find best model for the current operation
+    5.5 Analyze pre-existing road network graph
+ 0. Exit""")
 
         option = input("Choice: ")
         print()
