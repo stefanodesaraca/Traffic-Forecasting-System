9d14a0d7c20a919a962c392a0061fb0e270997bf
stefanodesaraca <stefanodesaraca@gmail.com>
2025-02-25 17:04:18 +0100

    Other important updates

--- DataCleaning.py
+++ DataCleaning.py
@@ -17,6 +17,15 @@ class Cleaner:
 
 
     @staticmethod
+    def import_TRPs(file):
+        with open(file, "r") as TRPs:
+
+            trp_info = json.load(TRPs)
+
+            return trp_info
+
+
+    @staticmethod
     def import_data(file):
         with open(file, "r") as f:
             data = json.load(f)
@@ -24,9 +33,10 @@ class Cleaner:
         return data
 
 
-    #TODO USEFUL?
+    #General definition of the data_overview() function. This will take two different forms: the traffic volumes one and the average speed one.
+    #Thus, the generic "data" parameter will become the volumes_data or the avg_speed_data one
     @staticmethod
-    def data_overview(data: dict, verbose: bool):
+    def data_overview(trp_data: dict, data: dict, verbose: bool):
         return data
 
 
@@ -38,38 +48,39 @@ class TrafficVolumesCleaner(Cleaner):
 
 
     #This function is only to give the user an overview of the data which we're currently cleaning, and some specific information about the TRP (Traffic Registration Point) which has collected it
-    def data_overview(self, data: dict, verbose: bool):
-        data = data["trafficRegistrationPoints"]
+    def data_overview(self, trp_data: dict, volumes_data: dict, verbose: bool):
+
+        volumes_data = volumes_data["trafficData"]
 
         if verbose is True:
 
             print("******** Traffic Registration Point Information ********")
 
-            print("ID: ", data["id"])
-            print("Name: ", data["name"])
-            print("Road category: ", data["location"]["roadReference"]["roadCategory"]["id"])
-            print("Coordinates -> ", "Lat: ", data["location"]["latLon"]["lat"], "Lon: ", data["location"]["latLon"]["lon"])
-            print("County name: ", data["location"]["county"]["name"])
-            print("County number: ", data["location"]["county"]["number"])
-            print("Geographic number: ", data["location"]["county"]["geographicNumber"])
-            print("Country part: ", data["location"]["county"]["countryPart"]["name"])
-            print("Municipality ID: ", data["location"]["municipality"]["id"])
-            print("Municipality name: ", data["location"]["municipality"]["name"])
+            print("ID: ", trp_data["id"])
+            print("Name: ", trp_data["name"])
+            print("Road category: ", trp_data["location"]["roadReference"]["roadCategory"]["id"])
+            print("Coordinates -> ", "Lat: ", volumes_data["location"]["latLon"]["lat"], "Lon: ", trp_data["location"]["latLon"]["lon"])
+            print("County name: ", trp_data["location"]["county"]["name"])
+            print("County number: ", trp_data["location"]["county"]["number"])
+            print("Geographic number: ", trp_data["location"]["county"]["geographicNumber"])
+            print("Country part: ", trp_data["location"]["county"]["countryPart"]["name"])
+            print("Municipality ID: ", trp_data["location"]["municipality"]["id"])
+            print("Municipality name: ", trp_data["location"]["municipality"]["name"])
 
-            print("Traffic registration type: ", data["trafficRegistrationType"])
+            print("Traffic registration type: ", trp_data["trafficRegistrationType"])
             print("Data time span: ")
-            print(" - First data: ", data["dataTimeSpan"]["firstData"])
-            print(" - First data with quality metrics: ", data["dataTimeSpan"]["firstDataWithQualityMetrics"])
+            print(" - First data: ", trp_data["dataTimeSpan"]["firstData"])
+            print(" - First data with quality metrics: ", trp_data["dataTimeSpan"]["firstDataWithQualityMetrics"])
             print(" - Latest data: ")
-            print("   > Volume by day: ", data["dataTimeSpan"]["latestData"]["volumeByDay"])
-            print("   > Volume by hour: ", data["dataTimeSpan"]["latestData"]["volumeByHour"])
-            print("   > Volume average daily by year: ", data["dataTimeSpan"]["latestData"]["volumeAverageDailyByYear"])
-            print("   > Volume average daily by season: ", data["dataTimeSpan"]["latestData"]["volumeAverageDailyBySeason"])
-            print("   > Volume average daily by month: ", data["dataTimeSpan"]["latestData"]["volumeAverageDailyByMonth"])
+            print("   > Volume by day: ", trp_data["dataTimeSpan"]["latestData"]["volumeByDay"])
+            print("   > Volume by hour: ", trp_data["dataTimeSpan"]["latestData"]["volumeByHour"])
+            print("   > Volume average daily by year: ", trp_data["dataTimeSpan"]["latestData"]["volumeAverageDailyByYear"])
+            print("   > Volume average daily by season: ", trp_data["dataTimeSpan"]["latestData"]["volumeAverageDailyBySeason"])
+            print("   > Volume average daily by month: ", trp_data["dataTimeSpan"]["latestData"]["volumeAverageDailyByMonth"])
 
-            data = dd.DataFrame(data)
+            volumes_data = dd.DataFrame(volumes_data)
 
-            print(data.describe())
+            print(volumes_data.describe())
 
 
 
@@ -77,8 +88,8 @@ class TrafficVolumesCleaner(Cleaner):
 
             print("******** Traffic Registration Point Information ********")
 
-            print("ID: ", data["id"])
-            print("Name: ", data["name"])
+            print("ID: ", trp_data["id"])
+            print("Name: ", trp_data["name"])
 
         return None
 
@@ -91,6 +102,8 @@ class TrafficVolumesCleaner(Cleaner):
 
         #Importing a single json file to be cleaned
         volumes = self.import_data(file)
+
+        print(volumes)
 
         self.data_overview(volumes, verbose=True)
 
@@ -110,7 +123,12 @@ class TrafficVolumesCleaner(Cleaner):
 
         print("Raw traffic volumes files: ", volume_files, "\n\n")
 
-        self.cleaning_pipeline(traffic_volumes_folder_path + volume_files[0])
+
+        traffic_registration_points = self.import_TRPs(f"{self._cwd}/{self._ops_folder}/{self._ops_name}/{self._ops_name}_data/traffic_measurement_points.json")
+
+        #TODO TEST HERE WITH [:2]
+        for volume_f in volume_files[:2]:
+            self.cleaning_pipeline(traffic_volumes_folder_path + volume_f) #String concatenation here
 
 
         return None
