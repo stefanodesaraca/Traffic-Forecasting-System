7557682c037a1eb1bab655475cb54febc7f277b0
stefanodesaraca <stefanodesaraca@gmail.com>
2025-02-27 13:48:49 +0100

    Code cleaning

--- tfs_data_downloader.py
+++ tfs_data_downloader.py
@@ -1,16 +1,204 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 import json
 import os
-from tfs_data_fetcher import *
 from warnings import simplefilter
 import math
 import numpy as np
-import time
 from tqdm import tqdm
 
 simplefilter("ignore")
 
 ops_folder = "ops"
 cwd = os.getcwd()
+
+
+# --------------------------------- GraphQL Client Start ---------------------------------
+
+
+def start_client():
+    transport = AIOHTTPTransport(url="https://trafikkdata-api.atlas.vegvesen.no/")
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    return client
+
+
+
+# --------------------------------- GraphQL Queries Section (Data Fetching) ---------------------------------
+
+
+#The number 3 indicates the Oslo og Viken county, which only includes the Oslo municipality
+def fetch_traffic_measurement_points(client: Client):
+
+    tmp_query = gql(
+        '''
+        {
+          trafficRegistrationPoints(
+            searchQuery: {roadCategoryIds: [E, R, F, K, P], countyNumbers: [3], isOperational: true, trafficType: VEHICLE, registrationFrequency: CONTINUOUS}
+          ) {
+            id
+            name
+            location {
+              coordinates {
+                latLon {
+                  lat
+                  lon
+                }
+              }
+              roadReference{
+                roadCategory{
+                  id
+                }
+              }
+              roadLinkSequence{
+                roadLinkSequenceId
+                relativePosition
+              }
+              county{
+                name
+                number
+                geographicNumber
+                countryPart{
+                  id
+                  name
+                }
+              }
+              municipality{
+                name
+                number
+              }
+            }
+            trafficRegistrationType            
+            dataTimeSpan {
+              firstData
+              firstDataWithQualityMetrics
+              latestData {
+                volumeByDay
+                volumeByHour
+                volumeAverageDailyByYear
+                volumeAverageDailyBySeason
+                volumeAverageDailyByMonth
+              }
+            }
+          }
+        }
+        ''')
+
+    traffic_measurement_points = client.execute(tmp_query)
+
+    return traffic_measurement_points
+
+
+def fetch_traffic_volumes_for_tmp_id(client: Client, traffic_measurement_point: str, time_start: str, time_end: str):
+
+    tv_query = gql(f"""{{
+        trafficData(trafficRegistrationPointId: "{traffic_measurement_point}") {{
+            volume {{
+                byHour(from: "{time_start}", to: "{time_end}") {{
+                    edges {{
+                        node {{
+                            from
+                            to
+                            total {{
+                                volumeNumbers {{
+                                    volume
+                                }}
+                                coverage {{
+                                    percentage
+                                }}
+                            }}
+                            byLane {{
+                                lane {{
+                                    laneNumberAccordingToRoadLink
+                                    laneNumberAccordingToMetering
+                                }}
+                                total {{
+                                    coverage {{
+                                        percentage
+                                    }}
+                                    volumeNumbers {{
+                                        volume
+                                    }}
+                                }}
+                            }}
+                            byDirection {{
+                                heading
+                                total {{
+                                    coverage {{
+                                        percentage
+                                    }}
+                                    volumeNumbers {{
+                                        volume
+                                    }}
+                                }}
+                            }}
+                        }}
+                    }}
+                    pageInfo {{
+                        hasNextPage
+                        endCursor
+                    }}
+                }}
+            }}
+        }}
+    }}""")
+
+    traffic_volumes = client.execute(tv_query)
+    #print(traffic_volumes)
+
+    return traffic_volumes
+
+
+def fetch_road_categories(client: Client):
+
+    rc_query = gql('''
+    {
+        roadCategories{
+            id
+            name
+        }
+    }
+    ''')
+
+    road_categories = client.execute(rc_query)
+
+    return road_categories
+
+
+def fetch_areas(client: Client):
+
+    a_query = gql('''
+    {
+      areas {
+        countryParts {
+          name
+          id
+          counties {
+            name
+            number
+            geographicNumber
+            municipalities {
+              name
+              number
+            }
+            countryPart {
+              name
+              id
+            }
+          }
+        }
+      }
+    }
+    ''')
+
+    areas = client.execute(a_query)
+
+    return areas
+
+
+
+# --------------------------------- JSON Writing Section ---------------------------------
+
 
 def traffic_measurement_points_to_json(ops_name: str):
     """
@@ -57,6 +245,8 @@ def traffic_volumes_data_to_json(ops_name: str, time_start: str, time_end: str):
         except TimeoutError:
             print("\033[91mTimeout Error Raised. Safely Exited the Program\033[0m")
             exit()
+
+        #TODO WATCH OUT FOR OUT OF MEMORY ERRORS, IF THE DATA IS TOO BIG FOR THE RAM THIS COULD CAUSE ERRORS
 
 
     requestChunkSize = int(math.sqrt(len(ids))) #The chunk size of each request cycle will be equal to the square root of the total number of ids
