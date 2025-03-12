4312521c57143870fd078cc60911d3cc6b0f31cf
stefanodesaraca <stefanodesaraca@gmail.com>
2025-03-12 10:33:03 +0100

    Important changes for average speed data cleaning. Since hour_start is of type "int" it's necessary to remove the '' from the .query() method

--- tfs_cleaning.py
+++ tfs_cleaning.py
@@ -634,11 +634,11 @@ class AverageSpeedCleaner(Cleaner):
             #print(day_data)
 
             for h in day_data["hour_start"].unique():
-                print(day_data[["mean_speed", "hour_start"]].query(f"hour_start == '{h}'")["mean_speed"]) #TODO WHY IS THIS AN EMPTY SERIES?
+                print(day_data[["mean_speed", "hour_start"]].query(f"hour_start == {h}")["mean_speed"]) #TODO WHY IS THIS AN EMPTY SERIES?
                 #Using the median to have a more robust indicator which won't be influenced by outliers as much as the mean
-                agg_data["mean_speed"].append(np.round(np.median(day_data[["mean_speed", "hour_start"]].query(f"hour_start == '{h}'")["mean_speed"]), decimals=2))
-                agg_data["percentile_85"].append(np.round(np.median(day_data[["percentile_85", "hour_start"]].query(f"hour_start == '{h}'")["percentile_85"]), decimals=2))
-                agg_data["coverage"].append(np.round(np.median(day_data[["coverage", "hour_start"]].query(f"hour_start == '{h}'")["coverage"]), decimals=2))
+                agg_data["mean_speed"].append(np.round(np.median(day_data[["mean_speed", "hour_start"]].query(f"hour_start == {h}")["mean_speed"]), decimals=2))
+                agg_data["percentile_85"].append(np.round(np.median(day_data[["percentile_85", "hour_start"]].query(f"hour_start == {h}")["percentile_85"]), decimals=2))
+                agg_data["coverage"].append(np.round(np.median(day_data[["coverage", "hour_start"]].query(f"hour_start == {h}")["coverage"]), decimals=2))
                 agg_data["hour_start"].append(h)
                 agg_data["year"].append(datetime.strptime(str(ud)[:10], "%Y-%m-%d").strftime("%Y"))
                 agg_data["month"].append(datetime.strptime(str(ud)[:10], "%Y-%m-%d").strftime("%m"))
