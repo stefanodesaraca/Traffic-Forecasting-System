eb948f2f69a2b5f5ad0bae3cc5815e124ade768b
stefanodesaraca <stefanodesaraca@gmail.com>
2025-03-15 18:17:27 +0100

    Updates on weeks encoding for ML

--- tfs_ml.py
+++ tfs_ml.py
@@ -72,11 +72,26 @@ class TrafficVolumesForecaster:
         volumes["hour_sin"] = sin_transformer(data=volumes["hour"], timeframe=24)
         volumes["hour_cos"] = sin_transformer(data=volumes["hour"], timeframe=24)
 
+        volumes["week_sin"] = sin_transformer(data=volumes["week"], timeframe=52)
+        volumes["week_cos"] = sin_transformer(data=volumes["week"], timeframe=52)
+
         volumes["day_sin"] = sin_transformer(data=volumes["day"], timeframe=31)
         volumes["day_cos"] = sin_transformer(data=volumes["day"], timeframe=31)
 
         volumes["month_sin"] = sin_transformer(data=volumes["month"], timeframe=12)
         volumes["month_cos"] = sin_transformer(data=volumes["month"], timeframe=12)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         print(volumes)
