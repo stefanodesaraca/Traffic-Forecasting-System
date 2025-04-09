aaa7d3e53bf2facc8601fc37b19c001e79cfcf70
stefanodesaraca <stefanodesaraca@gmail.com>
2025-04-09 12:16:18 +0200

    Gathering ideas to improve utilities functions

--- tfs_forecasting_settings.py
+++ tfs_forecasting_settings.py
@@ -3,22 +3,19 @@ from datetime import datetime
 import os
 
 target_datetime_filename = "target_datetime"
-dt_format = "%Y-%m-%dT%H:%M:%S"  # Datetime format, the hour (H) must be zero-padded and 24-h base, for example: 01, 02, ..., 12, 13, 14, 15, etc.
+forecasting_dt_format = "%Y-%m-%dT%H"  # Datetime format, the hour (H) must be zero-padded and 24-h base, for example: 01, 02, ..., 12, 13, 14, 15, etc.
+#In this case we'll only ask for the hour value since, for now, it's the maximum granularity for the predictions we're going to make
 cwd = os.getcwd()
 
 
 def write_forecasting_target_datetime() -> None:
 
-    dt = str(input("Insert Target Datetime (YYYY-MM-DDTHH:MM:SS): ")) #The month number must be zero-padded, for example: 01, 02, etc.
+    dt = str(input("Insert Target Datetime (YYYY-MM-DDTHH): ")) #The month number must be zero-padded, for example: 01, 02, etc.
 
     if check_datetime(dt) is True:
         print("Target datetime set to: ", dt, "\n\n")
-
-        with open(f"{target_datetime_filename}.txt", "w") as t_dt_writer:
-            t_dt_writer.write(dt)
-
+        with open(f"{target_datetime_filename}.txt", "w") as t_dt_writer: t_dt_writer.write(dt)
         return None
-
     else:
         print("\033[91mWrong datetime format, try again\033[0m")
         exit()
@@ -28,10 +25,8 @@ def read_forecasting_target_datetime() -> datetime:
 
     try:
         with open(f"{target_datetime_filename}.txt", "r") as t_dt_reader:
-
             target_dt = t_dt_reader.read()
-            target_dt = datetime.strptime(target_dt, dt_format)
-
+            target_dt = datetime.strptime(target_dt, forecasting_dt_format)
     except FileNotFoundError:
         print("\033[91mTarget Datetime File Not Found\033[0m")
         exit()
@@ -44,7 +39,6 @@ def del_forecasting_target_datetime() -> None:
     try:
         os.remove(cwd + "/" + target_datetime_filename + ".txt")
         print("Target datetime file deleted successfully\n\n")
-        
     except FileNotFoundError:
         print("\033[91mTarget Datetime File Not Found\033[0m")
         exit()
