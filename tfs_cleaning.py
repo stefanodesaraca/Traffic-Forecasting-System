ba659fb9dc3186ee326939a3bd3105d9d6460b8a
stefanodesaraca <stefanodesaraca@gmail.com>
2025-05-10 16:48:40 +0200

    Updating and improving by_lane data parsing

--- tfs_cleaning.py
+++ tfs_cleaning.py
@@ -79,7 +79,8 @@ class BaseCleaner:
         return pd.DataFrame(mice_imputer.fit_transform(data), columns=data.columns) # Fitting the imputer and processing all the data columns except the date one #TODO BOTTLENECK
 
 
-    def _is_empty(self, data: dict[Any, Any]) -> bool:
+    @staticmethod
+    def _is_empty(data: dict[Any, Any]) -> bool:
         return True if len(data) == 0 else False
 
 
@@ -138,15 +139,18 @@ class TrafficVolumesCleaner(BaseCleaner):
             return None
 
 
-    def _get_lanes_number(self, data: dict[Any, Any]) -> int:
+    @staticmethod
+    def _get_lanes_number(data: dict[Any, Any]) -> int:
         return max((ln["lane"]["laneNumberAccordingToRoadLink"] for ln in data[0]["node"]["byLane"]))  # Determining the total number of lanes for the TRP taken into consideration. Using a generator comprehension to improve performances
 
 
-    def _get_directions(self, data: dict[Any, Any]) -> Generator[int]:
+    @staticmethod
+    def _get_directions(data: dict[Any, Any]) -> Generator[int]:
         return (d["heading"] for d in data[0]["node"]["byDirection"])
 
 
-    def _get_registration_datetimes(self, edges: dict[Any, Any]) -> set[str]:
+    @staticmethod
+    def _get_registration_datetimes(edges: dict[Any, Any]) -> set[str]:
         return set(datetime.fromisoformat(node["node"]["from"]).replace(tzinfo=None).isoformat() for node in edges) # Only keeping the datetime without the +00:00 at the end
                # Removing duplicates with set() at the end
 
@@ -259,7 +263,28 @@ class TrafficVolumesCleaner(BaseCleaner):
         return pd.DataFrame(by_hour_structured).sort_values(by=["date", "hour"], ascending=True)
 
 
-    def _parse_by_lane(self, payload: dict[Any, Any]) -> dict[Any, Any] | None:
+
+    def _parse_by_lane(self, payload: dict[Any, Any]) -> pd.DataFrame | dd.DataFrame | None:
+
+        trp_id = payload["trafficData"]["trafficRegistrationPoint"]["id"]
+        payload = payload["trafficData"]["volume"]["byHour"]["edges"]
+
+        if self._is_empty(payload):
+            print(f"\033[91mNo data found for TRP: {trp_id}\033[0m\n\n")
+            return None
+
+        update_metainfo(trp_id, ["common", "non_empty_volumes_trps"], mode="append")
+
+        n_lanes = self._get_lanes_number(payload)
+        print("Number of lanes: ", n_lanes)
+        directions = list(self._get_directions(payload))
+        print("Directions available: ", directions)
+
+        # Just for demonstration purposes
+        registration_dates = self._get_registration_datetimes(payload)
+        print("Number of days where registrations took place: ", len(registration_dates))
+        print("First registration day available: ", min(registration_dates))
+        print("Last registration day available: ", max(registration_dates))
 
         by_lane_structured = {
             "trp_id": [],
@@ -274,14 +299,46 @@ class TrafficVolumesCleaner(BaseCleaner):
             "lane": [],
         }
 
+        missing_days_cnt = 0
+        for d, mh in self._get_missing_data(payload).items():
+            for h in mh:
+                by_lane_structured["trp_id"].append(trp_id)
+                by_lane_structured["volume"].append(None)
+                by_lane_structured["coverage"].append(None)
+                by_lane_structured["year"].append(datetime.strptime(d, "%Y-%m-%d").strftime("%Y"))
+                by_lane_structured["month"].append(datetime.strptime(d, "%Y-%m-%d").strftime("%m"))
+                by_lane_structured["week"].append(datetime.strptime(d, "%Y-%m-%d").strftime("%V"))
+                by_lane_structured["day"].append(datetime.strptime(d, "%Y-%m-%d").strftime("%d"))
+                by_lane_structured["hour"].append(h)
+                by_lane_structured["date"].append(d)
 
+            missing_days_cnt += 1
 
+        print("Number of missing days: ", missing_days_cnt)
 
+        for edge in payload:
 
+            # This is the datetime which will be representative of a volume, specifically, there will be multiple datetimes with the same day
+            # to address this fact we'll just re-format the data to keep track of the day, but also maintain the volume values for each hour
+            reg_datetime = datetime.strptime(datetime.fromisoformat(edge["node"]["from"]).replace(tzinfo=None).isoformat(),"%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%dT%H")  # Only keeping the datetime without the +00:00 at the end
 
+            # Every lane's data is kept isolated from the other lanes' data, so a for cycle is needed to extract all the data from each lane's section
+            for lane in edge["node"]["byLane"]:
 
+                # ------- Extracting data from the dictionary and appending it into by_lane_structured -------
 
-        return
+                by_lane_structured["trp_id"].append(trp_id)
+                by_lane_structured["year"].append(datetime.strptime(reg_datetime, "%Y-%m-%dT%H").strftime("%Y"))
+                by_lane_structured["month"].append(datetime.strptime(reg_datetime, "%Y-%m-%dT%H").strftime("%m"))
+                by_lane_structured["week"].append(datetime.strptime(reg_datetime, "%Y-%m-%dT%H").strftime("%V"))
+                by_lane_structured["day"].append(datetime.strptime(reg_datetime, "%Y-%m-%dT%H").strftime("%d"))
+                by_lane_structured["hour"].append(datetime.strptime(reg_datetime, "%Y-%m-%dT%H").strftime("%H"))
+                by_lane_structured["volume"].append(lane["total"]["volumeNumbers"]["volume"] if lane["total"]["volumeNumbers"] is not None else None) # In some cases the volumeNumbers key could have null as value, so the "volume" key won't be present. In that case we'll directly insert None as value with an if statement
+                by_lane_structured["coverage"].append(lane["total"]["coverage"]["percentage"] if lane["total"]["coverage"] is not None else None)
+                by_lane_structured["lane"].append(lane["lane"]["laneNumberAccordingToRoadLink"])
+                by_lane_structured["date"].append(datetime.strptime(reg_datetime, "%Y-%m-%dT%H").date().isoformat())
+
+        return pd.DataFrame(by_lane_structured).sort_values(by=["date", "hour"], ascending=True)
 
 
 
@@ -334,32 +391,6 @@ class TrafficVolumesCleaner(BaseCleaner):
 
 
 
-
-        # pprint.pprint(by_hour_structured)
-
-
-            #   ----------------------- By lane section -----------------------
-
-            lanes_data = node["node"]["byLane"]  # Extracting byLane data
-
-            # Every lane's data is kept isolated from the other lanes' data, so a for cycle is needed to extract all the data from each lane's section
-            for lane in lanes_data:
-                road_link_lane_number = lane["lane"]["laneNumberAccordingToRoadLink"]
-                lane_volume = (lane["total"]["volumeNumbers"]["volume"] if lane["total"]["volumeNumbers"] is not None else None)  # In some cases the volumeNumbers key could have null as value, so the "volume" key won't be present. In that case we'll directly insert None as value with an if statement
-                lane_coverage = (lane["total"]["coverage"]["percentage"] if lane["total"]["coverage"] is not None else None)
-
-                # ------- Extracting data from the dictionary and appending it into by_lane_structured -------
-
-                by_lane_structured["trp_id"].append(trp_id)
-                by_lane_structured["year"].append(year)
-                by_lane_structured["month"].append(month)
-                by_lane_structured["week"].append(week)
-                by_lane_structured["day"].append(day)
-                by_lane_structured["hour"].append(hour)
-                by_lane_structured["volume"].append(lane_volume)
-                by_lane_structured["coverage"].append(lane_coverage)
-                by_lane_structured["lane"].append(road_link_lane_number)
-                by_lane_structured["date"].append(date)
 
             #   ----------------------- By direction section -----------------------
 
