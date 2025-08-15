bc4619fbce48d916d1a4d31fcbbae53320e165ac
stefanodesaraca <stefanodesaraca@gmail.com>
2025-08-15 12:59:39 +0200

    Updates on BatchStreamLoader 1

--- src/loaders.py
+++ src/loaders.py
@@ -22,7 +22,8 @@ class BatchStreamLoader:
                    limit: PositiveInt | None = None
                    ) -> dd.DataFrame:
         df_partitions = []
-        with self._db_broker.get_stream(sql=f"""
+        #TODO WITH IS THE PROBLEM
+        stream = self._db_broker.get_stream(sql=f"""
             SELECT 
                 v.trp_id AS trp_id,
                 v.volume AS volume,
@@ -35,12 +36,20 @@ class BatchStreamLoader:
             AND {'t.road_category = ANY(%s)' if road_category_filter else "1=1"}
             ORDER BY "zoned_dt_iso" DESC
             {f"LIMIT {limit}" if limit else ""}
-        """, filters=tuple(f for f in [trp_list_filter, road_category_filter] if f), batch_size=batch_size, row_factory="tuple_row") as stream_cursor:
+        """, filters=tuple(f for f in [trp_list_filter, road_category_filter] if f), batch_size=batch_size, row_factory="tuple_row")
         # The ORDER BY (descending order) is necessary since in time series forecasting the order of the records is fundamental
 
         # WARNING: the order of the filters in the list within the list comprehension must be the same as the order of conditions inside the sql query
-            while rows := stream_cursor.fetchmany(500):
-                df_partitions.append(dd.from_pandas(pd.DataFrame.from_records(rows)))
+        batch = []
+        for row in stream:
+            batch.append(row)
+            if len(batch) >= 100000:
+                df_partitions.append(dd.from_pandas(pd.DataFrame.from_records(batch), npartitions=1))
+                batch = []
+
+        # Adding remaining rows
+        if batch:
+            df_partitions.append(dd.from_pandas(pd.DataFrame.from_records(batch), npartitions=1))
 
         return dd.concat(dfs=df_partitions, axis=1).sort_values(ascending=True, by=["zoned_dt_iso"])
 
@@ -52,7 +61,7 @@ class BatchStreamLoader:
                        limit: PositiveInt | None = None
                        ) -> dd.DataFrame:
         df_partitions = []
-        with self._db_broker.get_stream(sql=f"""
+        stream = self._db_broker.get_stream(sql=f"""
              SELECT 
                  ms.trp_id AS trp_id,
                  ms.mean_speed AS mean_speed,
@@ -66,12 +75,20 @@ class BatchStreamLoader:
             AND {'t.road_category = ANY(%s)' if road_category_filter else "1=1"}
             ORDER BY "zoned_dt_iso" DESC
             {f"LIMIT {limit}" if limit else ""}
-        """, filters=tuple(f for f in [trp_list_filter, road_category_filter] if f), batch_size=batch_size, row_factory="tuple_row") as stream_cursor:
+        """, filters=tuple(f for f in [trp_list_filter, road_category_filter] if f), batch_size=batch_size, row_factory="tuple_row")
         # The ORDER BY (descending order) is necessary since in time series forecasting the order of the records is fundamental
 
         # WARNING: the order of the filters in the list within the list comprehension must be the same as the order of conditions inside the sql query
-            while rows := stream_cursor.fetchmany(500):
-                df_partitions.append(dd.from_pandas(pd.DataFrame.from_records(rows)))
+        batch = []
+        for row in stream:
+            batch.append(row)
+            if len(batch) >= 100000:
+                df_partitions.append(dd.from_pandas(pd.DataFrame.from_records(batch), npartitions=1))
+                batch = []
+
+        # Adding remaining rows
+        if batch:
+            df_partitions.append(dd.from_pandas(pd.DataFrame.from_records(batch), npartitions=1))
 
         return dd.concat(dfs=df_partitions, axis=1).sort_values(ascending=True, by=["zoned_dt_iso"])
 
